4275bfe72fa99dea04cdcc2674746c8d2e7ad43b
Matthew Robertson <matthewrobertson03@gmail.com>
2013-11-12 21:48:53 -0800

    can quit

--- src/irc_test.erl
+++ src/irc_test.erl
@@ -4,21 +4,32 @@
 
 -export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
--export([start/0]).
+-export([start/0, stop/1]).
+
+-record(state, {socket}).
 
 
 start() ->
   lager:info("Starting IRC test"),
   State = {},
-	gen_server:start_link({local, ?MODULE}, ?MODULE, State, []).
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-init(State) ->
+stop(Pid) ->
+  gen_server:call(Pid, stop).
+
+init([]) ->
   lager:info("Init IRC test"),
-  {ok, State}.
+  {ok, Socket} = gen_tcp:connect("irc.freenode.net", 6667, [binary, {active,true}]),
+  gen_tcp:send(Socket, "Nick matthew_1\r\n"),
+  gen_tcp:send(Socket, "User matthew_1 8 * : Matthew Robertson\r\n"),
+  {ok, #state{socket=Socket}}.
 
 code_change(_OldVersion, State, _Extra) -> {ok, State}.
 
-handle_call(Req, _Caller, State) -> 
+handle_call(stop, _Caller, State) ->
+  gen_tcp:send(State#state.socket, "QUIT :erlang bitches!!!\r\n"),
+  {stop, normal, ok, State};
+handle_call(Req, _Caller, State) ->
   lager:info("Call IRC test"),
   {reply, sweet, State}.
 
@@ -26,11 +37,14 @@ handle_cast(Req, State) ->
   lager:info("Cast IRC test"),
   {noreply, State}.
 
-handle_info(Req, State) -> 
+handle_info({tcp, _Port, Stuff}, State) ->
+  lager:info("~p", [Stuff]),
+  {noreply, State};
+handle_info(Req, State) ->
   lager:info("Info IRC test"),
   {noreply, State}.
 
-terminate(_Reason, _State) -> 
+terminate(_Reason, _State) ->
   lager:info("Terminate IRC test"),
   ok.
 
